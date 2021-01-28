66616ae13cbc4fa4786aa65797842dda41c97bdc
Daniel <danielc@runninghill.co.za>
2021-01-28 16:21:54 +0200

    Truncate long text on note-cards

--- notes-frontend/src/app/note-card/note-card.component.ts
+++ notes-frontend/src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCardComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('truncator', {static: true}) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText',  {static: true}) bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    console.log(this.bodyText);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+
   }
 
 }
